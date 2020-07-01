1e2d4e1714d6712a3adce3539a6ea49325da820c
Khair Muhammad <36230386+khairmuhammad@users.noreply.github.com>
2020-07-01 18:32:10 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,137 @@
+import json
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import plotly.offline as py
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+from plotly.figure_factory import create_table
+import plotly.express as px
+
+app = dash.Dash(__name__)
+server = app.server
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+gapminder = px.data.gapminder()
+
+dff = gapminder.copy()
+#List of dataset columns
+available_indicators = list(gapminder)
+
+#list of countries
+country_test = list(gapminder.country)
+countries = [] 
+[countries.append(x) for x in country_test if x not in countries]
+
+#list of years
+years = list(gapminder.year)
+
+
+# In[4]:
+fig = px.scatter(dff, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+           size="pop", color="continent", hover_name="country", facet_col="continent",
+           log_x=True, size_max=45, range_x=[100,100000], range_y=[25,90])
+		   
+
+fig1 = px.choropleth(gapminder, locations="iso_alpha", color="lifeExp", hover_name="country", animation_frame="year",
+              color_continuous_scale=px.colors.sequential.Plasma, projection="natural earth")
+
+app.layout = html.Div([
+	html.H1("Dashboard for My Visualization Work done in Plotly Express", style={'textAlign': 'center', 'color': 'Black'}),
+	
+	
+	html.H2("Scatter Plot", style={'textAlign': 'center', 'color': 'Blue'}),
+	html.P("This scatter plot shows the continent-wise growth of life expectancy and GDP per Capita of countries. Along with that it also shows population, country name, continent and life expectancy on hovering.",
+	style={'textAlign': 'center'}),
+    dcc.Graph(id='graph-with-slider'),
+	html.P("Slide the years below to see the scatter plot for that year",
+	style={'textAlign': 'center'}),
+    dcc.Slider(
+        id='year-slider',
+        min=gapminder['year'].min(),
+        max=gapminder['year'].max(),
+        value=gapminder['year'].min(),
+        marks={str(year): str(year) for year in gapminder['year'].unique()},
+        step=None
+    ),
+	
+	
+	html.Br(),
+	html.H2("Bar Chart", style={'textAlign': 'center', 'color': 'Red'}),
+	html.P("This bar chart shows the yearly life expectancy of the selected country. Along with that it also shows population and gdp per capita on hovering.",
+	style={'textAlign': 'center'}),
+	dcc.Graph(id='graph-with-dropdown'),
+    html.P("Select country for which you want to show Bar Plot",
+	style={'textAlign': 'center'}),
+	dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': i, 'value': i} for i in countries],
+        value=countries[0]
+    ),
+	
+	
+	html.Br(),
+	html.H2("Scatter Animation", style={'textAlign': 'center', 'color': 'Green'}),
+	html.P("This Animation shows the yearly life expectancy of the selected country. Along with that it also shows population and gdp per capita on hovering.",
+	style={'textAlign': 'center'}),
+	dcc.Graph(figure=fig),
+	
+	
+	html.Br(),
+	html.H2("Scatter Animation", style={'textAlign': 'center', 'color': 'Green'}),
+	html.P("This Animation shows the yearly life expectancy of the selected country. Along with that it also shows population and gdp per capita on hovering.",
+	style={'textAlign': 'center'}),
+	dcc.Graph(figure=fig1)
+	
+])
+
+
+
+
+#Callback for Scatter Plot
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_gapminder = gapminder[gapminder.year == selected_year]
+
+    fig = px.scatter(filtered_gapminder, x="gdpPercap", y="lifeExp", 
+                     size="pop", color="continent", hover_name="country", 
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+	
+	
+
+#Callback for Bar Plot
+@app.callback(
+    Output('graph-with-dropdown', 'figure'),
+    [Input('country-dropdown', 'value')])
+def update_figure(selected_country):
+    filtered_gapminder = gapminder[gapminder.country == selected_country]
+
+    fig = px.bar(filtered_gapminder, x='year', y='pop',
+             hover_data=['lifeExp', 'gdpPercap'], color='lifeExp',
+             labels={'pop':'population of Canada'}, height=400)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
